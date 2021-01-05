b295c26a9d17772ec4a3396fa72a837bd7bba02f
seri-code <75782257+seri-code@users.noreply.github.com>
2021-01-05 18:12:14 +0900

    Create DataAccessObject(beans).java

---
+++ mall7/DataAccessObject(beans).java
@@ -0,0 +1,60 @@
+package beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DataAccessObject {
+	protected Connection connection;
+	protected Statement statement;
+	protected PreparedStatement pstatement;
+	protected ResultSet rs;
+	
+	public DataAccessObject() {
+
+	}
+	// 오라클 연결  :: Connection 개체 생성
+		public void getConnection() {
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				connection = DriverManager.getConnection("jdbc:oracle:thin:@106.243.194.230:7005:xe", "sol", "1234");
+			}catch(Exception e) {
+
+			}
+		}
+		
+		public void setAutoCommit(boolean isAuto) {
+			try {
+				this.connection.setAutoCommit(isAuto);
+			} catch (SQLException e) {e.printStackTrace();}
+		}
+
+		// Transaction 처리 :: COMMIT  || ROLLBACK
+		public void setTransaction(boolean isCommit) {
+			try {
+				if(isCommit) {
+					this.connection.commit();
+				} else {
+					this.connection.rollback();
+				}
+			}catch(Exception e) {}
+		}
+
+		// 오라클 연결 해제 :: Connection.close()
+		public void closeConnection() {
+			try {
+				if(!this.connection.isClosed()) {
+					this.connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+}
+
+
+		
