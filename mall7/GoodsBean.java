adcf4c45ee9f9900b1ca9ef7ca82b3ad90a93b54
seri-code <75782257+seri-code@users.noreply.github.com>
2021-01-05 18:12:49 +0900

    Create GoodsBean.java

---
+++ mall7/GoodsBean.java
@@ -0,0 +1,62 @@
+package beans;
+
+public class GoodsBean {
+	private String word;
+	private String goCode;
+	private String goName;
+	private int	goPrice;
+	private int goStock;
+	private String goImage;
+	private String seCode;
+	private String seName;
+	
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public String getGoCode() {
+		return goCode;
+	}
+	public void setGoCode(String goCode) {
+		this.goCode = goCode;
+	}
+	public String getGoName() {
+		return goName;
+	}
+	public void setGoName(String goName) {
+		this.goName = goName;
+	}
+	public int getGoPrice() {
+		return goPrice;
+	}
+	public void setGoPrice(int goPrice) {
+		this.goPrice = goPrice;
+	}
+	public int getGoStock() {
+		return goStock;
+	}
+	public void setGoStock(int goStock) {
+		this.goStock = goStock;
+	}
+	public String getGoImage() {
+		return goImage;
+	}
+	public void setGoImage(String goImage) {
+		this.goImage = goImage;
+	}
+	public String getSeCode() {
+		return seCode;
+	}
+	public void setSeCode(String seCode) {
+		this.seCode = seCode;
+	}
+	public String getSeName() {
+		return seName;
+	}
+	public void setSeName(String seName) {
+		this.seName = seName;
+	}
+	
+}
